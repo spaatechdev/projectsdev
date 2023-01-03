127040b0189dfa3669b1dc04bd4bd296b13470b4
spaatechdev <jibeshn@spaatech.net>
2023-01-03 15:43:30 +0530

    settings changing

--- plux/plux/settings.py
+++ plux/plux/settings.py
@@ -257,9 +257,11 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@spaatech.net'
-EMAIL_HOST_PASSWORD = 'PROJECT#321'
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = "PROJECT#321"
+
+LOGIN_URL = '/'
