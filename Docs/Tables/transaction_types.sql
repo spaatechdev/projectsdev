ee18ccdb5fb83fa95328fc4eeba00d907976a3c0
spaatechdev <jibeshn@spaatech.net>
2023-01-05 13:19:33 +0530

    tables

--- Docs/Tables/transaction_types.sql
+++ Docs/Tables/transaction_types.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 24, 2022 at 06:46 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Host: localhost:3306
+-- Generation Time: Jan 05, 2023 at 12:48 AM
+-- Server version: 5.7.40-cll-lve
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `chakda_ply`
+-- Database: `chakda_ply_test`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `transaction_types` (
   `id` bigint(20) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `transaction_types`
@@ -43,8 +44,7 @@ INSERT INTO `transaction_types` (`id`, `name`, `deleted`) VALUES
 (3, 'Return', 0),
 (4, 'Transfer Out', 0),
 (5, 'Transfer In', 0),
-(6, 'Physical Stock', 0),
-(7, 'Invoice', 0);
+(6, 'Physical Stock', 0);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +64,7 @@ ALTER TABLE `transaction_types`
 -- AUTO_INCREMENT for table `transaction_types`
 --
 ALTER TABLE `transaction_types`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
